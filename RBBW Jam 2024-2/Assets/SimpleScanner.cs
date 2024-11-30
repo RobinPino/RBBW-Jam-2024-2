758af58e9945771211a93680d7c58bb059ab4be2
RobinPino <r.a.pino@student.tue.nl>
2024-11-30 12:31:45 +0100

    Got basic scanner shader working

---
+++ RBBW Jam 2024-2/Assets/SimpleScanner.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleScanner : MonoBehaviour
+{
+    [SerializeField] Material mat;
+    [SerializeField] float scanMaxRange;
+
+    [SerializeField] AnimationCurve RevealCurve;
+    [SerializeField] float revealDuration;
+    [SerializeField] AnimationCurve StopCurve;
+    [SerializeField] float stopDuration;
+
+    [ContextMenu("Start Scan")]
+    public void StartScan()
+    {
+        StartCoroutine(SmoothReveal(RevealCurve, revealDuration, 100));
+    }
+
+    [ContextMenu("Stop Scan")]
+    public void StopScan()
+    {
+        StartCoroutine(SmoothReveal(StopCurve, stopDuration, 100));
+    }
+
+    IEnumerator SmoothReveal(AnimationCurve curve, float duration, int steps)
+    {
+        float t = 0;
+        float step = duration / steps;
+        float currentValue;
+        while (t < duration)
+        {
+            currentValue = curve.Evaluate(t / duration * curve.keys[curve.length - 1].time) * scanMaxRange;
+            mat.SetFloat("_RevealRadius", currentValue);
+            print(t);
+            t += step;
+            yield return new WaitForSeconds(step);
+        }
+    }
+
+
+}
