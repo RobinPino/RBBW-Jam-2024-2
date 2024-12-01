9507556ac2d74a142f184381104d6bd54fa98633
Jomp375 <91837656+Jomp375@users.noreply.github.com>
2024-12-01 12:52:27 +0100

    created level 1 and 2

--- RBBW Jam 2024-2/Assets/Scripts/Shape.cs
+++ RBBW Jam 2024-2/Assets/Scripts/Shape.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Shapes", menuName = "ScriptableObjects/ShapeScriptableObject", order = 1)]
 public class Shape : ScriptableObject
 {
-    public int speed;
+    public float speed;
 	public enum Type	{Square,Triangle,Hexagon};
     public Type type;
     public Sprite sprite;
