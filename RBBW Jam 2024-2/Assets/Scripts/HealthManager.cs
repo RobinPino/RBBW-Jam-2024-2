5f63340d7b5935f04ff10c91a4608f42d6fd03af
RobinPino <r.a.pino@student.tue.nl>
2024-12-01 16:28:54 +0100

    Added nice hearts

--- RBBW Jam 2024-2/Assets/Scripts/HealthManager.cs
+++ RBBW Jam 2024-2/Assets/Scripts/HealthManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
     public int Counter = 0;
     public GameObject[] Hearts;
+    [SerializeField] Sprite deadHeartSprite;
+    [SerializeField] Sprite fullHeartSprite;
+
+    private void Awake()
+    {
+        foreach (GameObject heart in Hearts)
+        {
+            heart.GetComponent<Image>().sprite = fullHeartSprite;
+        }
+    }
+
     public void removeHeart()
     {
-        Hearts[Counter].SetActive(false);
+        Hearts[Counter].GetComponent<Image>().sprite = deadHeartSprite;
         Counter++;
     }
 }
